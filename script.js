1531044ad583f18c73ada5c3b743186c368ad2c0
Rahul-Bhatt-CS <rbhatt.cs@gmail.com>
2025-04-04 00:26:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const cursor = document.querySelector('.cursor');
 document.addEventListener('mousemove',(e) => {
-cursor.style.left = e.pageX + 'px';
-cursor.style.top = e.pageY + 'px';
+cursor.style.left = e.clientX + 'px';
+cursor.style.top = e.clientY + 'px';
 })  
     function checkData(){
         var fname = document.getElementById("fname");
